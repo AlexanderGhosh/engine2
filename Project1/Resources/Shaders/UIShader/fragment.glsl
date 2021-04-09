c5d2652ce12b7e71be36a4ca5fb714eeb3873054
AlexanderGhosh <ghoshalexander@gmail.com>
2021-04-10 00:43:44 +0100

    uirender shader uses MatItem3

--- Project1/Resources/Shaders/UIShader/fragment.glsl
+++ Project1/Resources/Shaders/UIShader/fragment.glsl
@@ -4,13 +4,13 @@ out vec4 FragColor;
 
 in vec2 texCoords;
 
-uniform sampler2D col_id;
-uniform vec4 col_vec;
+struct MatItem3 {
+    vec3 raw;
+    sampler2D id;
+    float mixValue;
+};
+uniform MatItem3 col;
 
 void main() {
-    vec3 col = col_vec.rgb;
-    if (col_vec.a == 0){
-        col = texture(col_id, texCoords).rgb;
-    }
-    FragColor = vec4(1);
+    FragColor = vec4(mix(col.raw, texture(col.id, texCoords).rgb, col.mixValue), 1);
 }
