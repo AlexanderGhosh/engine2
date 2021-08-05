98c5c82601bc9ad7384c23a722d7127d5285830e
AlexanderGhosh <ghoshalexander@gmail.com>
2021-08-06 00:26:17 +0100

    removed reduntendt stuff in main and fiex some bugs

--- Project1/Main.cpp
+++ Project1/Main.cpp
@@ -123,7 +123,7 @@ int main() {
     const Primative::Model cubeBuffer  = ResourceLoader::createModel("Resources/Models/cube.obj"); // needed for the skybox
     const Primative::Model planeBuffer = ResourceLoader::createModel("Resources/Models/plane.dae");
     // const Primative::Model minikitBuffer = ResourceLoader::createModel("Resources/Models/minikit.fbx");
-    const Primative::Model orbBuffer = ResourceLoader::createModel("Resources/Models/sphere.obj");
+    // const Primative::Model orbBuffer = ResourceLoader::createModel("Resources/Models/sphere.obj");
     //const Primative::Model cityBuffer = ResourceLoader::createModel("Resources/Models/city.obj");
     timer.log();
 
@@ -132,41 +132,41 @@ int main() {
     timer.log();
 
     timer.start("Textures");
-    const unsigned wi   = ResourceLoader::loadTexture("Resources/Textures/window.png", TextureType::AlbedoMap, 0);
-    const unsigned wiy  = ResourceLoader::loadTexture("Resources/Textures/yellowWindow.png", TextureType::AlbedoMap, 0);
+    // const unsigned wi   = ResourceLoader::loadTexture("Resources/Textures/window.png", TextureType::AlbedoMap, 0);
+    // const unsigned wiy  = ResourceLoader::loadTexture("Resources/Textures/yellowWindow.png", TextureType::AlbedoMap, 0);
     // const unsigned hm       = ResourceLoader::loadTexture("Resources/Textures/heightmap.png", TextureType::HeightMap, 0);
-    const unsigned rainDrop = ResourceLoader::loadTexture("Resources/Textures/raindrop.png", TextureType::AlbedoMap, 0);
-    const unsigned dirt     = ResourceLoader::loadTexture("Resources/Textures/dirt.png", TextureType::AlbedoMap, 0);
+    // const unsigned rainDrop = ResourceLoader::loadTexture("Resources/Textures/raindrop.png", TextureType::AlbedoMap, 0);
+    // const unsigned dirt     = ResourceLoader::loadTexture("Resources/Textures/dirt.png", TextureType::AlbedoMap, 0);
     //const unsigned cityTex  = ResourceLoader::loadTexture("Resources/Textures/City Tex.png", TextureType::AlbedoMap, 0);
 
-    auto grassMaterialInfo = ResourceLoader::createPBRInfo("Resources/Textures/Grass", { TextureType::AlbedoMap, TextureType::AOMap, TextureType::MetalicMap, TextureType::NormalMap, TextureType::RoughnessMap }, { 0, 0, 0, 0, 0 });
-    auto& grassMaterial = ResourceLoader::createPBR(grassMaterialInfo);
-    grassMaterial.setRepeatValue(10);
-
-    auto waterMaterialInfo = ResourceLoader::createPBRInfo("Resources/Textures/Water", { TextureType::AlbedoMap, TextureType::RoughnessMap, TextureType::NormalMap, TextureType::AOMap, TextureType::MetalicMap }, { 0, 0, 0, 0, 0 });
-    auto& waterMaterial = ResourceLoader::createPBR(waterMaterialInfo);
-    Materials::MatItemSingle<glm::vec4> waterAlbedo({ 0, 0, 1, 1 });
-    Materials::MatItemSingle<glm::vec3> waterEmission({ 0, 0, 0 });
-    Materials::MatItemSingle<float> waterMetalic(0.5f);
-    Materials::MatItemSingle<float> waterRoughness(0.5f);
-    Materials::MatItemSingle<float> waterAO(0.5f);
-
-    waterMaterial.setAlbedo(&waterAlbedo);
-    waterMaterial.setEmission(&waterEmission);
-    waterMaterial.setMetalic(&waterMetalic);
-    waterMaterial.setRoughness(&waterRoughness);
-    waterMaterial.setAO(&waterAO);
+    // auto grassMaterialInfo = ResourceLoader::createPBRInfo("Resources/Textures/Grass", { TextureType::AlbedoMap, TextureType::AOMap, TextureType::MetalicMap, TextureType::NormalMap, TextureType::RoughnessMap }, { 0, 0, 0, 0, 0 });
+    // auto& grassMaterial = ResourceLoader::createPBR(grassMaterialInfo);
+    // grassMaterial.setRepeatValue(10);
+    // 
+    // auto waterMaterialInfo = ResourceLoader::createPBRInfo("Resources/Textures/Water", { TextureType::AlbedoMap, TextureType::RoughnessMap, TextureType::NormalMap, TextureType::AOMap, TextureType::MetalicMap }, { 0, 0, 0, 0, 0 });
+    // auto& waterMaterial = ResourceLoader::createPBR(waterMaterialInfo);
+    // Materials::MatItemSingle<glm::vec4> waterAlbedo({ 0, 0, 1, 1 });
+    // Materials::MatItemSingle<glm::vec3> waterEmission({ 0, 0, 0 });
+    // Materials::MatItemSingle<float> waterMetalic(0.5f);
+    // Materials::MatItemSingle<float> waterRoughness(0.5f);
+    // Materials::MatItemSingle<float> waterAO(0.5f);
+    // 
+    // waterMaterial.setAlbedo(&waterAlbedo);
+    // waterMaterial.setEmission(&waterEmission);
+    // waterMaterial.setMetalic(&waterMetalic);
+    // waterMaterial.setRoughness(&waterRoughness);
+    // waterMaterial.setAO(&waterAO);
   
     timer.log();
 
     timer.start("Objects");
 
 
-    GameObject orb(glm::vec3(0, 1.5, 0));
-    Component::RenderMesh orbMesh = Component::RenderMesh();
-    orbMesh.setModel(orbBuffer);
-    orbMesh.setMaterial(&waterMaterial);
-    orb.addComponet(&orbMesh);
+    // GameObject orb(glm::vec3(0, 1.5, 0));
+    // Component::RenderMesh orbMesh = Component::RenderMesh();
+    // orbMesh.setModel(orbBuffer);
+    // orbMesh.setMaterial(&waterMaterial);
+    // orb.addComponet(&orbMesh);
 
     /*Particles::DomeDistribution distribution = Particles::DomeDistribution(1.0f);
     Component::ParticleEmmiter emmiter = Component::ParticleEmmiter(10, 10, true, 0.25);
@@ -177,31 +177,6 @@ int main() {
 
     timer.log();
 
-    timer.start("Terrain"); 
-
-    const int landSize = 2;
-    const float scale = 100;
-    const int landRes = 100;
-    std::vector<Terrain> allLand;
-    allLand.reserve(landSize * landSize);
-    for (char i = 0; i < landSize; i++) {
-        for (char j = 0; j < landSize; j++) {
-
-            Terrain land(landRes);
-            land.renderWireframe = false;
-
-            land.setPosition(glm::vec3(i * scale, -0.5f, j * scale));
-            land.setScale({ scale, 10, scale });
-            // land.setNoiseBuffer(Utils::NoiseGeneration::getMap(glm::vec3(i, j, 0), landRes + 1, { 1, 0.5, 0.1 }, { 1, 2, 3 }));
-            land.useTextureMap(false);
-            land.setMaterial(&grassMaterial);
-
-            allLand.push_back(land);
-        }
-    }
-    timer.log();
-
-
 
     timer.start("Player");
     GameObject player = GameObject(glm::vec3(0, 0, 5));
@@ -210,75 +185,13 @@ int main() {
     Component::CharacterController cc;
     player.addComponet(&cc);
     PlayerControler playerScript;
-    playerScript.ground = allLand;
     player.addComponet(&playerScript);
     DebugScreen debugScript;
     player.addComponet(&debugScript);
     Component::AudioReciever recieverComp;
     player.addComponet(&recieverComp);
 
-
-    GameObject lightSource(glm::vec3(0, 1.5, -2.5));
-    Component::PointLight light(glm::vec3(1), 100.0f);
-    lightSource.addComponet(&light);
-
-    GameObject lightSource2(glm::vec3(0, 3.75, 0));
-    Component::DirectionalLight light2 = Component::DirectionalLight({ 1, 1, 1 }, { 1, 1, 1 }, 1);
-    lightSource2.addComponet(&light2);
-    Component::ShadowCaster shadowCaster = Component::ShadowCaster(&light2, 15);
-    lightSource2.addComponet(&shadowCaster);
-
-
-    Materials::MatItemSingle<glm::vec4> windowAlbedo(wi);
-    Materials::MatItemSingle<glm::vec3> windowNormal(glm::vec3(0, 1, 0));
-    Materials::MatItemSingle<glm::vec3> windowEmission(glm::vec3(0, 1, 0));
-    Materials::MatItemSingle<float> windowMetalic(0.5f);
-    Materials::MatItemSingle<float> windowRoughness(0.0f);
-    Materials::MatItemSingle<float> windowAO(0.5f);
-
-    Materials::PBR windowMat1(&windowAlbedo, &windowNormal, &windowEmission, &windowMetalic, &windowRoughness, &windowAO);
-
-    Materials::MatItemSingle<glm::vec4> windowAlbedo2(wiy);
-
-    Materials::PBR windowMat2(&windowAlbedo2, &windowNormal, &windowEmission, &windowMetalic, &windowRoughness, &windowAO);
-
-    GameObject window1(glm::vec3(0, 3.5, -3));
-    Component::RenderMesh windowMesh1;
-    windowMesh1.setModel(planeBuffer);
-    windowMesh1.setMaterial(&windowMat1);
-    windowMesh1.setTransparent(true);
-    window1.addComponet(&windowMesh1);
-
     timer.log();
-
-
-    Materials::MatItemSingle<glm::vec4> uiColourCanvas(glm::vec4(0.5, 1, 1, 0.5));
-    GUI::GUICanvas canvas;
-    canvas.setDimentions(SCREEN_DIMENTIONS / 2);
-    canvas.setBaseAlbedo(&uiColourCanvas);
-    canvas.setCornerRadius(50);
-    GUI::GUITextBlock element1 = GUI::GUITextBlock("Ya Mum");
-    GUI::GUIConstraint elementConstraints;
-    elementConstraints.setScreenDimentions(SCREEN_DIMENTIONS);
-    GUI::PercentConstraint percents = GUI::PercentConstraint(0.25);
-    GUI::PixelConstraint pixels = GUI::PixelConstraint(100);
-    GUI::SpanConstraint span = GUI::SpanConstraint();
-    // elementConstraints.setX(&pixels);
-    elementConstraints.setY(&percents);
-    elementConstraints.setWidth(&span);
-    elementConstraints.setHeight(&pixels);
-    element1.setConstraints(&elementConstraints);
-    Materials::MatItemSingle<glm::vec4> uiColour(glm::vec4(0.5, 0.0, 1, 1));
-    Materials::MatItemSingle<glm::vec4> uiColourBar(glm::vec4(1, 0.0, 0, 1));
-    Materials::MatItemSingle<glm::vec4> uiColourPointer(glm::vec4(0, 0.0, 1, 1));
-    element1.setAlbedo(&uiColour);
-    element1.setTextColour(&uiColourPointer);
-    // element1.setPointerAlbedo(&uiColourPointer);
-    // element1.setSliderAlbedo(&uiColourBar);
-
-
-    canvas.addElement(&element1);
-
 
     timer.start("Scene");
 
@@ -289,19 +202,8 @@ int main() {
     scene.initalize();
 
     scene.addObject(&player);
-    scene.addObject(&lightSource);
-    scene.addObject(&lightSource2);
-    scene.addObject(&window1);
-    //scene.addObject(&city);
 
-    scene.addObject(&orb);
-    for (Terrain& land : allLand) {
-        scene.addTerrain(&land);
-    }
-    
-    scene.addUI(&canvas);
-
-    scene.setShadowCaster(&shadowCaster);
+    //scene.addObject(&orb);
 
     timer.log();
 
