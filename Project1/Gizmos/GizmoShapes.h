db1f43567cf0707cd4e1168855f6d7709936bc65
AlexanderGhosh <ghoshalexander@gmail.com>
2023-08-20 12:01:25 +0100

    added triangle change line to draw from p1 to to p2

--- Project1/Gizmos/GizmoShapes.h
+++ Project1/Gizmos/GizmoShapes.h
@@ -4,7 +4,7 @@
 
 namespace Gizmos {
 	enum class Types {
-		Point, Line, Circle, Cuboide, Sphere
+		Point, Line, Circle, Cuboide, Sphere, Triangle
 	};
 	class Gizmo {
 	protected:
@@ -28,29 +28,34 @@ namespace Gizmos {
 		virtual void setPosition(Vector3 pos);
 		virtual void setColour(Vector3 colour);
 	};
+
 	class Point : public Gizmo {
 	public:
 		Point();
 		Point(Vector3 pos, Vector3 col);
 		~Point() = default;
-		void draw();
-		void cleanUp();
-		Types getType() const;
+		void draw() override;
+		void cleanUp() override;
+		Types getType() const override;
 	};
+
 	class Line : public Gizmo {
 	private:
-		glm::vec3 leftOffset, rightOffset;
+		glm::vec3 p1_, p2_;
 	public:
 		Line();
 		Line(Vector3 pos, Vector3 col);
 		Line(Vector3 from, Vector3 to, bool over);
 		~Line() = default;
-		void draw();
-		void cleanUp();
-		Types getType() const;
+		void draw() override;
+		void cleanUp() override;
+		Types getType() const override;
 		void setLeftOffset(Vector3 left);
 		void setRightOffset(Vector3 right);
+
+		void setPosition(Vector3 pos) override;
 	};
+
 	class Circle : public Gizmo {
 	private:
 		float radius;
@@ -59,12 +64,13 @@ namespace Gizmos {
 		Circle();
 		Circle(Vector3 pos, Vector3 col);
 		~Circle() = default;
-		void draw();
-		void cleanUp();
-		Types getType() const;
+		void draw() override;
+		void cleanUp() override;
+		Types getType() const override;
 		void setRadius(Float r);
 		void setUp(Vector3 up);
 	};
+
 	class Cuboide : public Gizmo {
 	private:
 		glm::vec3 dimensions;
@@ -73,11 +79,12 @@ namespace Gizmos {
 		Cuboide();
 		Cuboide(Vector3 pos, Vector3 col);
 		~Cuboide() = default;
-		void draw();
-		void cleanUp();
-		Types getType() const;
+		void draw() override;
+		void cleanUp() override;
+		Types getType() const override;
 		void setDimentions(Vector3 dim);
 	};
+
 	class Sphere : public Gizmo {
 	private:
 		float radius;
@@ -87,11 +94,28 @@ namespace Gizmos {
 		Sphere();
 		Sphere(Vector3 pos, Vector3 col);
 		~Sphere() = default;
-		void draw();
-		void cleanUp();
-		Types getType() const;
+		void draw() override;
+		void cleanUp() override;
+		Types getType() const override;
 		void setRadius(Float r);
-		void setPosition(Vector3 pos);
-		void setColour(Vector3 colour);
+		void setPosition(Vector3 pos) override;
+		void setColour(Vector3 colour) override;
+	};
+
+	class Triangle : public Gizmo {
+	private:
+		std::array<Line, 3> lines;
+		std::vector<Point> points;
+	public:
+		Triangle();
+		Triangle(Vector3 p1, Vector3 p2, Vector3 p3, bool drawPoints);
+		~Triangle() = default;
+
+		void draw() override;
+		void cleanUp() override;
+		Types getType() const override;
+
+		void setPosition(Vector3 pos) override;
+		void setColour(Vector3 colour) override;
 	};
 }
